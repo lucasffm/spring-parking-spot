7d4ec437746a501becd96034bb3892819e3f724a
Lucas Mendes <lucasffm@gmail.com>
2023-06-29 02:35:11 -0300

    First Commit with Spring

---
+++ src/test/java/com/api/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,12 @@
+package com.api.parkingcontrol;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
